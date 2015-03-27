912a54b04a9d896af1b5ae8b51af831f5fa0e454
Addison Zhang <zhangl3@rose-hulman.edu>
2015-03-27 17:14:20 -0400

    Added test2 - pass

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 
 public class PrimeFactorization {
 
-	public PrimeFactorization(){
-		
+	public PrimeFactorization() {
+
 	}
+
 	public static ArrayList<Integer> primeFactorization(int num) {
-		return new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		return (num == 2)?list: new ArrayList<Integer>();
+		
 	}
 
 }
--- src/PrimeFactorizationTest.java
+++ src/PrimeFactorizationTest.java
@@ -6,13 +6,15 @@ import org.junit.Test;
 public class PrimeFactorizationTest {
 	PrimeFactorization test = new PrimeFactorization();
 	ArrayList<Integer> list = new ArrayList<Integer>();
+	
 	@Test
 	public void test1() {
-		assertEquals(test.primeFactorization(1),list);
+		assertEquals(list,test.primeFactorization(1));
 	}
 	
 	@Test
 	public void test2() {
-		assertEquals(test.primeFactorization(2),list.add(2));
+		list.add(2);
+		assertEquals(list,test.primeFactorization(2));
 	}
 }
