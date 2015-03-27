250580d619a7574280c5929cc8c2060b81326f8d
Addison Zhang <zhangl3@rose-hulman.edu>
2015-03-27 16:35:32 -0400

    Added test2 - failed

--- src/PrimeFactorizationTest.java
+++ src/PrimeFactorizationTest.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 import org.junit.Test;
 public class PrimeFactorizationTest {
 	PrimeFactorization test = new PrimeFactorization();
-
+	ArrayList<Integer> list = new ArrayList<Integer>();
 	@Test
 	public void test1() {
-		assertEquals(test.primeFactorization(1),new ArrayList<Integer>());
+		assertEquals(test.primeFactorization(1),list);
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(test.primeFactorization(2),list.add(2));
 	}
 }
