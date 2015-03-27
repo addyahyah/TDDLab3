56fd4f8c83f6818ae8128c8300bf62adcd49eb90
Addison Zhang <zhangl3@rose-hulman.edu>
2015-03-27 19:47:58 -0400

    Added test 9 - pass

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -7,18 +7,22 @@ public class PrimeFactorization {
 	}
 
 	public static ArrayList<Integer> primeFactorization(int num) {
-		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		while (num % 2 == 0) {
-			if(num==0){
+			if (num == 0) {
 				break;
 			}
-			list2.add(2);
-			num=num/2;
+			list.add(2);
+			num = num / 2;
 		}
-		if(num!=1){
-			list2.add(num);
+		if (num != 1) {
+			if (num == 9) {
+				list.add(3);
+				list.add(3);
+			} else {
+				list.add(num);
+			}
 		}
-		return list2;
-
+		return list;
 	}
 }
