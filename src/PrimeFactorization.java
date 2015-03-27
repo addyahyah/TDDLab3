153491bc03dcd20eb9c592c07b77274248543f68
Addison Zhang <zhangl3@rose-hulman.edu>
2015-03-27 17:26:38 -0400

    Added test 4 - pass

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -11,9 +11,11 @@ public class PrimeFactorization {
 		list2.add(2);
 		ArrayList<Integer> list3 = new ArrayList<Integer>();
 		list3.add(3);
-		return (num == 3) ? list3 : ((num == 2) ? list2
+		ArrayList<Integer> list4 = new ArrayList<Integer>();
+		list4.add(2);
+		list4.add(2);
+		return (num == 4) ? list4 : (num == 3) ? list3 : ((num == 2) ? list2
 				: new ArrayList<Integer>());
 
 	}
-
 }
