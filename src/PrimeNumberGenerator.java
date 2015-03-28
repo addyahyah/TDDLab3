a2f6d7fdaa876849268923c4aab1fd351163f7c6
Addison Zhang <zhangl3@rose-hulman.edu>
2015-03-27 20:15:51 -0400

    Added test 1 - pass

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 
 public class PrimeNumberGenerator {
 
-	public PrimeNumberGenerator(){
+	public PrimeNumberGenerator() {
 	}
-	
-	public static ArrayList<Integer> primes(int num){
+
+	public static ArrayList<Integer> primes(int num) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(2);
+		if (num == 2) {
+			list.add(2);
+		}
 		return list;
 	}
-	
+
 	public Boolean validate(final Integer primeNumber) {
 		for (int i = 2; i < (primeNumber / 2); i++) {
 			if (primeNumber % i == 0) {
